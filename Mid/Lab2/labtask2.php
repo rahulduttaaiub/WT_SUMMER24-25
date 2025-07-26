055bf88e79ed4ef6543b81d1d82462854074ab82
rahuldutta2311@gmail.com <rahuldutta2311@gmail.com>
2025-07-26 10:54:16 +0600

    labtask2

---
+++ Mid/Lab2/labtask2.php
@@ -0,0 +1,70 @@
+<DOCTYPE html>
+<html lang="en">
+<head>
+<title>Bank Managment System</title>
+<link rel="stylesheet" href="labtask2.css">
+
+</head>
+
+<body>
+
+<center>
+<h1>Bank Management System</h1>
+<h3>Your Trusted Financial partner<h3>
+</center>
+<h4>Costomar Registation Form</h4>
+<div class="form-box">
+    <form>
+        <table>
+            <tr>
+                <td>Full Name:</td>
+                <td><input type="text"></td>
+</tr>
+<tr>
+    <td>Deth of birth:</td>
+    <td><input type ="text"></td>
+</tr>
+<tr>
+    <td>Gender:</td>
+    <td>
+<input type= "radio"name="gender">Male
+<input type= "radio"name="gender">Female
+<input type= "radio"name="gender">Other
+</td>
+</tr>
+<tr>
+    <td>Account Type:</td>
+    <td>
+<select>
+    <option>Saving</option>
+    <option>center</option>
+</select>
+</td>
+</tr>
+<tr>
+    <td>Initial Deposit:</td>
+    <td><input type="text"></td>
+    </tr>
+<tr>
+     <td>National Id:</td>
+      
+<td><input type="text"></td>
+  </tr>
+<tr>
+<td>Uplod Id Proof:</td>
+<td><input type="file"></td>
+    </tr>
+<tr>
+    <td colspan="2">
+        <input type="checkbox">I agre to the turms and condition
+        </td>
+</tr>
+<tr>
+    <td><button type="submit">Register</button></td>
+<td><button type="reset">Clear</button></td>
+</tr>
+</table>
+</form>
+</div>
+</body>
+</html>
